82bfbf933856ef4e0fcb9714d419553261ebc09b
sawar <sawar.sagwal@gmail.com>
2020-04-03 01:03:20 +0530

    Updates

---
+++ precache-manifest.8338ebf1eef114bddfa1a4078c650ff4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06013671c6109cf72db255fec477ebf8",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "7a2fd569d67db06320cb",
+    "url": "/Portfolio/static/css/main.c422acb5.chunk.css"
+  },
+  {
+    "revision": "5839d97feeaf929b1d9e",
+    "url": "/Portfolio/static/js/2.085c2d67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.085c2d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a2fd569d67db06320cb",
+    "url": "/Portfolio/static/js/main.4b8a64df.chunk.js"
+  },
+  {
+    "revision": "7ab249943e954d2343c8",
+    "url": "/Portfolio/static/js/runtime-main.736d31db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
